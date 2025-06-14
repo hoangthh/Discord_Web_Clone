82e49d50dee464cbdb6f9ae49498332a0b86facf
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-06-15 03:42:34 +0700

    refactor(server): config cors, global-pipes, cookies

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
