ce5995f9c564531b864d8e6dd725ce6e0577cbca
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-06-15 03:37:26 +0700

    refactor(server: app-module): remove unnecessary files, config auth, prisma

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProfileModule } from './profile/profile.module';
+import { ServerModule } from './server/server.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    PrismaModule,
+    ProfileModule,
+    ServerModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
