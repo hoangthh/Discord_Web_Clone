9e59b1fa80fcecbc000b1f493a4e5627c74da553
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-06-15 22:54:09 +0700

    refactor(client): config axios instance

--- client/api-client/axios-instance.ts
+++ client/api-client/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.BACKEND_URL || "http://localhost:3333",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   withCredentials: true,
 });
 
