1bd61ca3a374d84c75b0faa396b61f628a1efee5
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-06-18 22:33:18 +0700

    fix(server: auth-module): upload image from google auth to cloudinary

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ProfileModule } from 'src/profile/profile.module';
+import { CloudinaryModule } from 'src/services/cloudinary/cloudinary.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { GoogleStrategy } from './google.strategy';
-import { ProfileModule } from 'src/profile/profile.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { ProfileModule } from 'src/profile/profile.module';
     }),
     PassportModule,
     ProfileModule,
+    CloudinaryModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy],
