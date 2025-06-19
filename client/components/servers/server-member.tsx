3af87412ca2c94d4caa0dbf814d404ea72575a1d
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-06-19 22:23:19 +0700

    feat(client): render server channel adn server member list

---
+++ client/components/servers/server-member.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { MemberWithProfile, Role } from "@/models";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { UserAvatar } from "@/components/user-avatar";
+
+interface ServerMemberProps {
+  member: MemberWithProfile;
+  server: Server;
+}
+
+const roleIconMap = {
+  [Role.GUEST]: null,
+  [Role.MODERATOR]: <ShieldCheck className="ml-2 h-4 w-4 text-indigo-500" />,
+  [Role.ADMIN]: <ShieldAlert className="ml-2 h-4 w-4 text-rose-500" />,
+};
+
+export const ServerMember = ({ member, server }: ServerMemberProps) => {
+  const params = useParams<{ memberId: string; channelId: string }>();
+  const router = useRouter();
+
+  const icon = roleIconMap[member.role];
+  return (
+    <button
+      className={cn(
+        "group mb-1 flex w-full cursor-pointer items-center gap-x-2 rounded-md px-2 py-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50",
+        params.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700",
+      )}
+    >
+      <UserAvatar
+        src={member.profile.imageUrl}
+        className="h-8 w-8 md:h-8 md:w-8"
+      />
+      <p
+        className={cn(
+          "text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300",
+          params.channelId === member.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white",
+        )}
+      >
+        {member.profile.name}
+      </p>
+      {icon}
+    </button>
+  );
+};
---
+++ client/components/servers/server-section.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import {
+  Channel,
+  ChannelTypeEnum,
+  MemberRole,
+  Role,
+  ServerWithChannelWithMember,
+} from "@/models";
+import { ActionTooltip } from "../action-tooltip";
+import { Plus, Settings } from "lucide-react";
+import { useModal } from "@/hooks";
+
+interface ServerSectionProps {
+  label: string;
+  role?: MemberRole | string;
+  sectionType: "channels" | "members";
+  channelType?: ChannelTypeEnum | string;
+  server?: ServerWithChannelWithMember;
+}
+
+export const ServerSection = ({
+  label,
+  role,
+  sectionType,
+  channelType,
+  server,
+}: ServerSectionProps) => {
+  const { onOpen } = useModal();
+
+  return (
+    <div className="flex items-center justify-between py-2">
+      <p className="dark:text-zin-400 text-xs font-semibold text-zinc-500 uppercase">
+        {label}
+      </p>
+      {role !== Role.GUEST && sectionType === "channels" && (
+        <ActionTooltip label="Create Channel" side="top">
+          <button
+            onClick={() => onOpen("createChannel", { channelType })}
+            className="cursor-pointer text-zinc-500 transition hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300"
+          >
+            <Plus className="h-4 w-4" />
+          </button>
+        </ActionTooltip>
+      )}
+      {role === Role.ADMIN && sectionType === "members" && (
+        <ActionTooltip label="Manage Members" side="top">
+          <button
+            onClick={() => onOpen("members", { server })}
+            className="cursor-pointer text-zinc-500 transition hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300"
+          >
+            <Settings className="h-4 w-4" />
+          </button>
+        </ActionTooltip>
+      )}
+    </div>
+  );
+};
