eae5fc3f3811fb9e2bc8f65872fbd31b4ce49b12
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-06-19 20:53:50 +0700

    feat(client): add search channels and members modal

---
+++ client/components/servers/server-search.tsx
@@ -0,0 +1,103 @@
+"use client ";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { Search } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+interface ServerSearchProps {
+  data: {
+    label: string;
+    type: "channel" | "member";
+    data:
+      | {
+          icon: React.ReactNode;
+          name: string;
+          id: string;
+        }[]
+      | undefined;
+  }[];
+}
+
+export const ServerSearch = ({ data }: ServerSearchProps) => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams<{ serverId: string }>();
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && e.key === "f") {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handleClick = ({
+    id,
+    type,
+  }: {
+    id: string;
+    type: "channel" | "member";
+  }) => {
+    setOpen(false);
+
+    if (type === "member") {
+      router.push(`/servers/${params.serverId}/conversations/${id}`);
+    }
+    if (type === "channel") {
+      router.push(`/servers/${params.serverId}/channels/${id}`);
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={() => setOpen(true)}
+        className="group flex w-full cursor-pointer items-center gap-x-2 rounded-md px-2 py-2 transition hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50"
+      >
+        <Search className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />{" "}
+        <p className="text-sm font-semibold text-zinc-500 transition group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300">
+          Search
+        </p>
+        <kbd className="bg-muted text-muted-foreground pointer-events-none ml-auto inline-flex h-5 items-center gap-1 rounded border px-1.5 font-mono text-[10px] font-medium select-none">
+          <span>Ctrl</span>F
+        </kbd>
+      </button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search all channels and members"></CommandInput>
+        <CommandList>
+          <CommandEmpty>No Results found</CommandEmpty>
+          {data.map(({ label, type, data }) => {
+            if (!data?.length) return null;
+
+            return (
+              <CommandGroup key={label} heading={label}>
+                {data.map(({ id, icon, name }) => (
+                  <CommandItem
+                    key={id}
+                    className="cursor-pointer"
+                    onSelect={() => handleClick({ id, type })}
+                  >
+                    {icon}
+                    <span>{name}</span>
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            );
+          })}
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
--- client/components/servers/server-sidebar.tsx
+++ client/components/servers/server-sidebar.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import { ServerHeader, ServerSearch } from "@/components/servers";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useAuth, useServerByServerId } from "@/hooks";
-import { Channel, ChannelType, Member, MemberRole } from "@/models";
+import { Channel, ChannelType, Member, MemberRole, Role } from "@/models";
+import { Hash, Mic, ShieldAlert, ShieldCheck, VideoIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import { ServerHeader } from "@/components/servers";
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <VideoIcon className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+  [Role.GUEST]: null,
+  [Role.MODERATOR]: <ShieldCheck className="mr-2 h-4 w-4 text-indigo-500" />,
+  [Role.ADMIN]: <ShieldAlert className="mr-2 h-4 w-4 text-rose-500" />,
+};
 
 export const ServerSidebar = ({ serverId }: { serverId: string }) => {
-  const router = useRouter();
   const { profile, isLoading: profileLoading } = useAuth();
+  const router = useRouter();
   const {
     server,
     isLoading: serverLoading,
@@ -50,6 +64,50 @@ export const ServerSidebar = ({ serverId }: { serverId: string }) => {
           role={role as MemberRole}
         />
       )}
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Video Channels",
+                type: "channel",
+                data: videoChannels.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Members",
+                type: "member",
+                data: members.map((member) => ({
+                  id: member.id,
+                  name: member.profile.name,
+                  icon: roleIconMap[member.role],
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 };
