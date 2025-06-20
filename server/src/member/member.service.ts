28f9e8844859036b4266259985ae0ff1ecb20543
Tran Huy Hoang <tranhoang202204@gmail.com>
2025-06-20 20:40:18 +0700

    feat(server: member-module): add find first member of a server controller service

---
+++ server/src/member/member.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class MemberService {
+  constructor(private prisma: PrismaService) {}
+
+  async findFirstMemberByServerId({
+    serverId,
+    profileId,
+  }: {
+    serverId: string;
+    profileId: string;
+  }) {
+    const member = await this.prisma.member.findFirst({
+      where: {
+        serverId,
+        profileId,
+      },
+    });
+    return member;
+  }
+}
